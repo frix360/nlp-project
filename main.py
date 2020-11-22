38b0804f979ca8a977e2a88b06a485263c749d55
frix360 <ignas.dailyde@gmail.com>
2020-11-22 10:53:41 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def get_questions():
+    return [
+        'What is the size of a button?'
+    ]
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
