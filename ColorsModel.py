c57f5654ee7ecd0a777f5b8db15d4469e7964c07
Zygimantas <zygimantas@pop-os.localdomain>
2020-12-23 20:00:31 +0200

    Change model to match tutorial. New set.

--- ColorsModel.py
+++ ColorsModel.py
@@ -30,13 +30,13 @@ def predict(name):
     print(name + ',', 'R,G,B:', r, g, b)
 
 
-data = pandas.read_csv('set_1.csv')
+data = pandas.read_csv('set_0.csv')
 
 data.head()
 
 names = data["name"]
 
-maxlen = 40
+maxlen = 25
 t = Tokenizer(char_level=True)
 t.fit_on_texts(names)  # [1: 'I', 2, 3, 4]
 tokenized = t.texts_to_sequences(names)  # [sen, sen] -> [[1, 2, 3, 4], [4, 2, 3, 5]]
@@ -44,7 +44,6 @@ padded_names = preprocessing.sequence.pad_sequences(tokenized, maxlen=maxlen)
 
 one_hot_names = np_utils.to_categorical(padded_names)
 num_classes = one_hot_names.shape[-1]
-
 normalized_values = np.column_stack([norm(data["red"]), norm(data["green"]), norm(data["blue"])])
 
 model = Sequential()
@@ -55,15 +54,18 @@ model.add(Dense(3, activation='sigmoid'))
 model.compile(optimizer='adam', loss='mse', metrics=['acc'])
 model.summary()
 
-model.load_weights('model_2.h5')
+# model.load_weights('./models/model_2.h5')
+
+# color_name = 'Keras red'
+# predict(color_name)
+
+history = model.fit(one_hot_names, normalized_values,
+                    epochs=40,
+                    batch_size=32,
+                    validation_split=0.1)
+#
+
+model.save_weights('./models/model_3.h5')
 
 color_name = 'Keras red'
 predict(color_name)
-
-# history = model.fit(one_hot_names, normalized_values,
-#                     epochs=20,
-#                     batch_size=32,
-#                     validation_split=0.1)
-# #
-#
-# model.save_weights('model_2.h5')
